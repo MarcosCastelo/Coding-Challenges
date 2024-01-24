352bed1a7625b2aa6ae214e3ae069bb4b6e504b5
MarcosCastelo <marcosvitorcastelo@gmail.com>
2024-01-24 07:09:48 -0300

    create params filter

--- ccwc/main.go
+++ ccwc/main.go
@@ -1,41 +1,52 @@
 package main
 
 import (
-	"ccwc/counter"
-	"ccwc/utils"
 	"fmt"
+	"io"
 	"os"
 )
 
-var (
-	filename string
-	option   string
-	file     []byte
-)
-
-func init() {
-	if len(os.Args) > 1 {
-		filename = os.Args[1]
-	}
-
-	if len(os.Args) > 2 {
-		option = os.Args[2]
-	}
-	file = utils.GetFile(filename)
-}
-
 func main() {
-	switch option {
-	case "-c":
-		fmt.Println(counter.Bytes(file), filename)
-	case "-l":
-		fmt.Println(counter.Lines(file), filename)
-	case "-w":
-		fmt.Println(counter.Words(file), filename)
-	case "-m":
-		fmt.Println(counter.Chars(file), filename)
-	default:
-		fmt.Println(counter.Lines(file), counter.Words(file), counter.Bytes(file), filename)
-	}
-
+	content, _ := io.ReadAll(os.Stdin)
+	fmt.Println(string(content))
 }
+
+// import (
+// 	"ccwc/counter"
+// 	"ccwc/utils"
+// 	"fmt"
+// 	"os"
+// )
+
+// var (
+// 	filename string
+// 	option   string
+// 	file     []byte
+// )
+
+// func init() {
+// 	if len(os.Args) > 1 {
+// 		filename = os.Args[1]
+// 	}
+
+// 	if len(os.Args) > 2 {
+// 		option = os.Args[2]
+// 	}
+// 	file = utils.GetFile(filename)
+// }
+
+// func main() {
+// 	switch option {
+// 	case "-c":
+// 		fmt.Println(counter.Bytes(file), filename)
+// 	case "-l":
+// 		fmt.Println(counter.Lines(file), filename)
+// 	case "-w":
+// 		fmt.Println(counter.Words(file), filename)
+// 	case "-m":
+// 		fmt.Println(counter.Chars(file), filename)
+// 	default:
+// 		fmt.Println(counter.Lines(file), counter.Words(file), counter.Bytes(file), filename)
+// 	}
+
+// }
