31969d2379a5619314289c68e3d83c9d5f7c0a47
MarcosCastelo <marcosvitorcastelo@gmail.com>
2024-01-26 11:22:47 -0300

    remove unecessary files

--- ccwc/counter/bytes.go
+++
@@ -1,6 +0,0 @@
-package counter
-
-func Bytes(file []byte) (n int) {
-	n = len(file)
-	return
-}
--- ccwc/counter/lines.go
+++
@@ -1,9 +0,0 @@
-package counter
-
-import "strings"
-
-func Lines(file []byte) (n int) {
-	lines := strings.Split(string(file), "\n")
-	n = len(lines)
-	return
-}
